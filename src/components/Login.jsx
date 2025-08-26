a13a061733fc33aa62797f222fbc2d61d650986d
Harsh <gupta.harshit0699@gmail.com>
2025-08-26 18:48:30 +0530

    setup redux store and done with necessary installation

---
+++ src/components/Login.jsx
@@ -0,0 +1,77 @@
+import axios from "axios";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("harshit123@gmail.com");
+  const [password, setPassword] = useState("Harshit@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/login",
+        {
+          emailId,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(addUser(res.data));
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-20">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <div className="mt-4">
+              <label className="form-control w-full max-w-xs my-2">
+                <div className="label">
+                  <span className="label-text">Email ID</span>
+                </div>
+                <input
+                  type="text"
+                  value={emailId}
+                  className="input input-bordered w-full max-w-xs"
+                  onChange={(e) => setEmailId(e.target.value)}
+                />
+              </label>
+            </div>
+            <div className="my-5">
+              <label className="form-control w-full max-w-xs my-2">
+                <div className="label">
+                  <span className="label-text-alt">Password</span>
+                </div>
+                <input
+                  type="text"
+                  value={password}
+                  className="input input-bordered w-full max-w-xs"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </label>
+            </div>
+          </div>
+          <div className="card-actions justify-center mb-1 mt-6">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,29 @@
+import { useSelector } from "react-redux";
+
 const Navbar = () => {
+  const user = useSelector((store) => store.user);
+  console.log(user);
+
   return (
-     <div className="navbar bg-base-300">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">DevSphere</a>
-        </div>
+    <div className="navbar bg-base-300">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">DevSphere</a>
+      </div>
+      {user && (
         <div className="flex items-center gap-2">
-          <div className="form-control"></div>
+          <div className="form-control">Welcome, {user.firstName}</div>
           <div className="dropdown dropdown-end mx-7">
             <div
               tabIndex={0}
               role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
+              className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
+                <img alt="user photo" src={user.photoUrl} />
               </div>
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
               <li>
                 <a className="justify-between">
                   Profile
@@ -38,8 +39,9 @@ const Navbar = () => {
             </ul>
           </div>
         </div>
-      </div>
-  )
-}
+      )}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
